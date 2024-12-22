7b0b34938730f1d41edc80517984a4cc4d439cc0
Ediig Melchiior <ediigmelchiior@gmail.com>
2024-12-22 10:37:18 +0000

    add data for vercel

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import { Lusitana } from '@next/font/google';
+import {Inter} from '@next/font/google';
+
+// Importing the Lusitana font with specific weights
+export const lusitana = Lusitana({
+  subsets: ['latin'], // You can adjust subsets as needed
+  weight: ['400', '700'], // Choose the weights you need
+  display: 'swap', // Optional: Improves font loading
+});
+
+export const inter = Inter({
+    subsets: ['latin'], // You can adjust subsets as needed
+    weight: ['400', '700'], // Choose the weights you need
+    display: 'swap', // Optional: Improves font loading
+  });
